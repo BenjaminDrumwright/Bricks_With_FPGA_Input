f4732826979fc7fa4dc2a6f8f077529770dcfd41
hreaves <hreaves1@vols.utk.edu>
2025-05-08 16:22:16 -0400

    comments for paddle_controller.vhd

--- bricks/bricks.srcs/sources_1/new/paddle_controller.vhd
+++ bricks/bricks.srcs/sources_1/new/paddle_controller.vhd
@@ -1,3 +1,5 @@
+-- paddle_controller.vhd
+-- Takes buttons as input and sends out to python via uart (uart_tx.vhd)
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -6,25 +8,27 @@ entity paddle_controller is
     Port (
         clk     : in  std_logic;
         rst     : in  std_logic;
-        btnL    : in  std_logic;
-        btnR    : in  std_logic;
-        btnC    : in  std_logic;
-        btnT    : in  std_logic; -- Top for letter up
-        btnB    : in  std_logic; -- Bottom for letter down
-        uart_tx : out std_logic
+        btnL    : in  std_logic; -- Left button
+        btnR    : in  std_logic; -- Right button
+        btnC    : in  std_logic; -- Center button
+        btnT    : in  std_logic; -- Top button
+        btnB    : in  std_logic; -- Bottom button
+        uart_tx : out std_logic  -- 0 when uart is busy, 1 when ready
     );
 end paddle_controller;
 
 architecture Behavioral of paddle_controller is
-    signal tx_data   : std_logic_vector(7 downto 0) := (others => '0');
-    signal tx_start  : std_logic := '0';
-    signal tx_busy   : std_logic;
+    signal tx_data   : std_logic_vector(7 downto 0) := (others => '0'); --holds hex value to be sent across uart
+    signal tx_start  : std_logic := '0'; -- tells uart_tx to start processing tx_data
+    signal tx_busy   : std_logic;  -- tells if uart is busy sending data
     signal btnL_prev, btnR_prev, btnC_prev, btnT_prev, btnB_prev : std_logic := '0';
 begin
 
     process(clk)
     begin
+        -- Eveerything is synchronized to the clock edge
         if rising_edge(clk) then
+            -- Clear all data on reset
             if rst = '1' then
                 tx_start <= '0';
                 tx_data  <= (others => '0');
@@ -33,12 +37,13 @@ begin
                 btnC_prev <= '0';
                 btnT_prev <= '0';
                 btnB_prev <= '0';
+            -- If a button is high and wasn't before, set tx_data to hex value of corresponidng letter and start processing
             else
                 if btnL = '1' and btnL_prev = '0' then
-                    tx_data <= x"4C";  -- 'L'
+                    tx_data <= x"4C";  -- 'L' (Left for the paddle)
                     tx_start <= '1';
                 elsif btnR = '1' and btnR_prev = '0' then
-                    tx_data <= x"52";  -- 'R'
+                    tx_data <= x"52";  -- 'R' (Right for the paddle)
                     tx_start <= '1';
                 elsif btnC = '1' and btnC_prev = '0' then
                     tx_data <= x"53";  -- 'S' (Start or Select)
@@ -52,7 +57,7 @@ begin
                 else
                     tx_start <= '0';
                 end if;
-
+                -- Registers for button signals to ensure button doesn't work when held down
                 btnL_prev <= btnL;
                 btnR_prev <= btnR;
                 btnC_prev <= btnC;
@@ -61,15 +66,15 @@ begin
             end if;
         end if;
     end process;
-
+    -- Instantiation of uart_tx module to send tx_data across uart
     uart_inst : entity work.uart_tx
         port map (
             clk      => clk,
             rst      => rst,
-            tx_start => tx_start,
-            tx_data  => tx_data,
-            tx       => uart_tx,
-            tx_busy  => tx_busy
+            tx_start => tx_start, 
+            tx_data  => tx_data, 
+            tx       => uart_tx, 
+            tx_busy  => tx_busy 
         );
 end Behavioral;
 
